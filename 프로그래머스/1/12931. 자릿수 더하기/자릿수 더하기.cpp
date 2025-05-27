14b6b92d397c0dfa36ee1e093dbc24168f468922
Jo-sm94 <sungmin7410@gmail.com>
2025-05-27 14:55:48 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    while(n > 0){
+        int a = n % 10;
+        answer += a;
+        n /= 10;
+    }
+    
+    return answer;
+}
