f15bce1ab3df5b5c5f06a84eb4a0532739324525
Jo-sm94 <sungmin7410@gmail.com>
2025-06-04 09:17:25 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    
+    if(s.length() % 2 == 0){
+        answer = s.substr((s.length()/2)-1, 2);
+    }else{
+        answer = s.substr((s.length()/2), 1);
+    }
+    
+    return answer;
+}
