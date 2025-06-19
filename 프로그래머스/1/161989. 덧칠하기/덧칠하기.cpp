48960d29ec8ece8bb50478c417e097ffbbf6ec11
Jo-sm94 <sungmin7410@gmail.com>
2025-06-19 10:10:40 +0900

    [level 1] Title: 덧칠하기, Time: 0.25 ms, Memory: 5.71 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int tmp = 0;
+
+	
+	for (int i = 0; i < section.size(); i++) {
+		if (section[i] >= tmp) {
+			tmp = section[i] + m;
+			answer++;
+		}
+	}
+    
+    return answer;
+}
