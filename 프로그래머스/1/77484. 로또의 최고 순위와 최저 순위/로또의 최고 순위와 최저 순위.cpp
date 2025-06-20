48c9e6bc3061e82ce379a9e13c8cb471b1fa9dd3
Jo-sm94 <sungmin7410@gmail.com>
2025-06-20 09:34:59 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+    map<int, int> mp = {{6,1},{5,2},{4,3},{3,4},{2,5},{1,6},{0,6}};
+    int zeroCnt = 0;
+    int count = 0;
+    
+    for(int i : lottos){
+        if(i == 0) zeroCnt++;
+        for(int j : win_nums){
+            if(i == j){
+                count++;
+            }
+        }
+    }
+    
+    answer.push_back(mp[count+zeroCnt]);
+    answer.push_back(mp[count]);
+    
+    return answer;
+}
