f43ecb151ebe851d5ee0347ce9dff0a6ccc6949b
Jo-sm94 <sungmin7410@gmail.com>
2025-06-26 10:36:11 +0900

    Update 체육복.cpp

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
@@ -19,41 +18,23 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     }
     
     for(int i = 1; i <= n; i++){
-        cout << "=============" << endl;
-        cout << i << "  start" << endl;
         auto it = find(lost.begin(), lost.end(), i);      
         if(it == lost.end()){
             answer++;
-            cout << "체육복 있음 answer++" << endl;
         }else{
-            cout << "체육복 없음" << endl;
-            auto self = find(reserve.begin(), reserve.end(), i);
             auto prev = find(reserve.begin(), reserve.end(), i-1);
             auto next = find(reserve.begin(), reserve.end(), i+1);
-            /*
-            if(self != reserve.end()){
-                cout << "자신이 여벌의 체육복 있음 answer++" << endl;
+
+            if(prev != reserve.end()){
                 answer++;
-                reserve.erase(reserve.begin() + distance(reserve.begin(), self));
+                reserve.erase(reserve.begin() + distance(reserve.begin(), prev));
             }else{
-            */
-                if(prev != reserve.end()){
-                    cout << "앞사람이 여벌의 체육복 있음 answer++" << endl;
+                if(next != reserve.end()){
                     answer++;
-                    reserve.erase(reserve.begin() + distance(reserve.begin(), prev));
-                }else{
-                    if(next != reserve.end()){
-                        cout << "뒷사람이 여벌의 체육복 있음 answer++" << endl;
-                        answer++;
-                        reserve.erase(reserve.begin() + distance(reserve.begin(), next));
-                    }else{
-                        cout << "아무도 없음" << endl;
-                    }
+                    reserve.erase(reserve.begin() + distance(reserve.begin(), next));
                 }
-            //}
-            
-        }
-        
+            }       
+        }    
     }
     
     return answer;
