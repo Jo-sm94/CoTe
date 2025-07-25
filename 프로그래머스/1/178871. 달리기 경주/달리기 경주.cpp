cb7642541740c23fa57ce085c7c46d6e0dd0e8c1
Jo-sm94 <sungmin7410@gmail.com>
2025-07-25 09:26:37 +0900

    [level 1] Title: 달리기 경주, Time: 1028.60 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.cpp
@@ -1,13 +1,13 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
 vector<string> solution(vector<string> players, vector<string> callings) {
     vector<string> answer;
-    unordered_map<string, int> m;
+    map<string, int> m;
     
     for(int i = 0; i < players.size(); i++)
     {
