174eb73e6d53a6f6cab44adfcc1203afd065a6fa
Jo-sm94 <sungmin7410@gmail.com>
2025-08-05 09:05:53 +0900

    [level 2] Title: n^2 배열 자르기, Time: 28.96 ms, Memory: 24.2 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -10,9 +10,8 @@ vector<int> solution(int n, long long left, long long right) {
 
         int a = i/n;
         int b = i%n;
-     
+        
         answer.push_back(max(a, b) + 1);
-  
     }
    
     return answer;
