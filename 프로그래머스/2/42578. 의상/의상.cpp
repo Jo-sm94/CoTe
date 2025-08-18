2d0dc6069fbeb5ec5a3818b028a9deb86d8b664d
Jo-sm94 <sungmin7410@gmail.com>
2025-08-18 11:19:49 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,9 +1,23 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 0;
+    unordered_map<string, int> count;
+    
+    for (auto& c : clothes) {
+        count[c[1]]++;
+    }
+
+    int add = 1;
+    for (auto& pair : count) {
+        add *= (pair.second + 1);
+    }
+    
+    answer = add - 1;
+    
     return answer;
 }
