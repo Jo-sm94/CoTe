49cbe2c5474a5455a33c7fb0b6b2579d4426cdf5
Jo-sm94 <sungmin7410@gmail.com>
2025-09-18 09:56:44 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 5.39 ms, Memory: 40.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int zeroCount = 0;
+int oneCount = 0;
+
+void quad(int x, int y, int size, vector<vector<int>>& arr)
+{
+    int first = arr[x][y];
+    bool isSame = true;
+    
+    for(int i = x; i < x+size; ++i)
+    {
+        for(int j = y; j < y+size; ++j)
+        {
+            if(arr[i][j] != first)
+            {
+                isSame = false;
+                break;
+            }
+        }
+    }
+    
+    if(isSame)
+    {
+        if(first == 0)
+        {
+            zeroCount++;
+        }
+        else
+        {
+            oneCount++;   
+        }
+    }
+    else
+    {
+        int half = size / 2;
+        quad(x, y, half, arr);
+        quad(x, y+half, half, arr);
+        quad(x+half, y, half, arr);
+        quad(x+half, y+half, half, arr);
+    }
+}
+
+vector<int> solution(vector<vector<int>> arr) {
+    vector<int> answer;
+    
+    quad(0, 0, arr.size(), arr);
+    
+    answer.push_back(zeroCount);
+    answer.push_back(oneCount);
+    
+    return answer;
+}
