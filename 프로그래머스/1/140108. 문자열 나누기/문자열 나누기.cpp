c0817e39f881638ad3f9fe63636567b295afedc3
Jo-sm94 <sungmin7410@gmail.com>
2025-09-25 10:22:18 +0900

    [level 1] Title: 문자열 나누기, Time: 15.68 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -1,10 +1,47 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
+    int splitNum = 0;
+    int a = 0;
+    int b = 0;
+    
+    for(int i = 0; i < s.size(); ++i)
+    {
+        if(s[splitNum] == s[i])
+        {
+            a++;
+        }
+        else
+        {
+            b++;
+        }
+        cout << "a : "<< a << ",  b : " << b << endl;
+        if(a == b)
+        {
+            splitNum = i+1;
+            a = 0;
+            b = 0;
+            answer++;
+        }
+        
+        if(i == s.size()-1 && a != b)
+        {   
+            answer++;
+        }
+    }
+    
+    return answer;
+}
+
+
+
+/*
+int answer = 0;
     int splitNum = 0;
     bool check = true;
     
@@ -32,6 +69,4 @@ int solution(string s) {
             check = false;
         }
     }
-    
-    return answer;
-}
+*/
